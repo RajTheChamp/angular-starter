51a57fcce477287fc1b85985ba46a52e8b4a6961
RajTheChamp <rajasekhar.veeramalla@gmail.com>
2020-03-28 19:40:47 +0530

    initial commit

---
+++ src/app/library/library.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopNavComponent } from './top-nav/top-nav.component';
+
+
+
+@NgModule({
+  declarations: [TopNavComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LibraryModule { }
